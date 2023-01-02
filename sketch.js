8c4f4420a7b98b16457b5e50676a592b72f48b7c
SashaRoachakKapoor <121806975+SashaRoachakKapoor@users.noreply.github.com>
2023-01-02 17:25:43 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box
+
+function setup() {
+  createCanvas(400,400);
+box = createSprite (150,200,25,100)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+box.velocityX = 5
+}
+
+
+
+
